1f15d1a6197a1a42d8d1d6a5402d4ab67386b46f
SzachorJ <110390358+FilipPlaczek@users.noreply.github.com>
2023-06-22 15:30:09 +0200

    [02] update app files

---
+++ Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace inzynierkaapka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.AvailablePortsBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConnectPortButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.TextBoxValue1 = new System.Windows.Forms.TextBox();
+            this.TextBoxValue2 = new System.Windows.Forms.TextBox();
+            this.TextBoxValue3 = new System.Windows.Forms.TextBox();
+            this.TextBoxValue4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // AvailablePortsBox
+            // 
+            this.AvailablePortsBox.FormattingEnabled = true;
+            this.AvailablePortsBox.Location = new System.Drawing.Point(39, 75);
+            this.AvailablePortsBox.Name = "AvailablePortsBox";
+            this.AvailablePortsBox.Size = new System.Drawing.Size(121, 24);
+            this.AvailablePortsBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select COM port";
+            // 
+            // ConnectPortButton
+            // 
+            this.ConnectPortButton.Location = new System.Drawing.Point(65, 105);
+            this.ConnectPortButton.Name = "ConnectPortButton";
+            this.ConnectPortButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectPortButton.TabIndex = 2;
+            this.ConnectPortButton.Text = "Connect";
+            this.ConnectPortButton.UseVisualStyleBackColor = true;
+            this.ConnectPortButton.Click += new System.EventHandler(this.ConnectPortButton_Click);
+            // 
+            // TextBoxValue1
+            // 
+            this.TextBoxValue1.Location = new System.Drawing.Point(11, 192);
+            this.TextBoxValue1.Name = "TextBoxValue1";
+            this.TextBoxValue1.Size = new System.Drawing.Size(100, 22);
+            this.TextBoxValue1.TabIndex = 5;
+            // 
+            // TextBoxValue2
+            // 
+            this.TextBoxValue2.Location = new System.Drawing.Point(135, 192);
+            this.TextBoxValue2.Name = "TextBoxValue2";
+            this.TextBoxValue2.Size = new System.Drawing.Size(100, 22);
+            this.TextBoxValue2.TabIndex = 6;
+            // 
+            // TextBoxValue3
+            // 
+            this.TextBoxValue3.Location = new System.Drawing.Point(11, 244);
+            this.TextBoxValue3.Name = "TextBoxValue3";
+            this.TextBoxValue3.Size = new System.Drawing.Size(100, 22);
+            this.TextBoxValue3.TabIndex = 7;
+            // 
+            // TextBoxValue4
+            // 
+            this.TextBoxValue4.Location = new System.Drawing.Point(135, 244);
+            this.TextBoxValue4.Name = "TextBoxValue4";
+            this.TextBoxValue4.Size = new System.Drawing.Size(100, 22);
+            this.TextBoxValue4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "x-force";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "y-force";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "l-position";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(159, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "r-position";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(268, 13);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(535, 485);
+            this.zedGraphControl1.TabIndex = 14;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(832, 92);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "left position";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 17;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(1163, 92);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "right position";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 18;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 322);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 321);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 369);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 21;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(135, 370);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 351);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 16);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "min y value";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(135, 351);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "max y value";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 303);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 16);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "min x value";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(135, 302);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 16);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "max x value";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1475, 566);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBoxValue4);
+            this.Controls.Add(this.TextBoxValue3);
+            this.Controls.Add(this.TextBoxValue2);
+            this.Controls.Add(this.TextBoxValue1);
+            this.Controls.Add(this.ConnectPortButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AvailablePortsBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox AvailablePortsBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ConnectPortButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox TextBoxValue1;
+        private System.Windows.Forms.TextBox TextBoxValue2;
+        private System.Windows.Forms.TextBox TextBoxValue3;
+        private System.Windows.Forms.TextBox TextBoxValue4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using static System.Net.Mime.MediaTypeNames;
+using ZedGraph;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Security.Authentication;
+using System.Globalization;
+
+namespace inzynierkaapka
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort SerPort;
+        private string RecievedData;
+        private string[] SplitData;
+        private float xval;
+        private float minxval=0;
+        private float maxxval=0;
+        private float yval;
+        private float minyval=0;
+        private float maxyval=0;
+        private int lpos;
+        private int rpos;
+        
+
+        PointPairList gList = new PointPairList();
+        PointPairList lList = new PointPairList();
+        PointPairList rList = new PointPairList();
+        public Form1()
+        {
+            InitializeComponent();
+            InitGraph();
+            FetchAvailablePorts();
+        }
+        System.Windows.Forms.Timer PlotTimer = new System.Windows.Forms.Timer();
+        private void InitGraph()
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "g-force graph";
+            myPane.XAxis.Title.Text = "x-axis";
+            myPane.YAxis.Title.Text = "y-axis";
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            myPane.XAxis.Scale.Max = 4;
+            myPane.XAxis.Scale.Min = -4;
+            myPane.YAxis.Scale.Max = 4;
+            myPane.YAxis.Scale.Min = -4;
+
+            myPane.Chart.Fill = new Fill(Color.White,Color.Beige, 45.0f);
+            LineItem myCurve = myPane.AddCurve("", gList, Color.Blue, SymbolType.Circle);
+
+            myCurve.Line.IsVisible = true;
+            myCurve.Symbol.IsVisible = true;
+            myCurve.Symbol.Fill = new Fill(Color.Blue);
+            myCurve.Symbol.Size = 5;
+
+            chart1.ChartAreas[0].AxisY.Minimum= 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 180;
+            chart1.ChartAreas[0].AxisY.Interval = 10;
+            chart2.ChartAreas[0].AxisY.Minimum= 0;
+            chart2.ChartAreas[0].AxisY.Maximum = 180;
+            chart2.ChartAreas[0].AxisY.Interval = 10;
+
+        }
+
+        private void UpdateGraph()
+        {
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.Update();
+            zedGraphControl1.Refresh();
+   
+        }
+
+        private void ConvertAndFill()
+        {
+
+
+                chart1.Series[0].Points.Clear();
+                chart2.Series[0].Points.Clear();
+                xval = float.Parse(SplitData[1], CultureInfo.InvariantCulture.NumberFormat);
+                yval = 0-float.Parse(SplitData[2], CultureInfo.InvariantCulture.NumberFormat);
+                if(xval > maxxval) maxxval= xval;
+                if(xval < minxval) minxval= xval;
+                if(yval > maxyval) maxyval= yval;
+                if(yval < minyval) minyval= yval;
+                lpos = int.Parse(SplitData[3]);
+                rpos = int.Parse(SplitData[4]);
+
+            textBox2.Text = maxxval.ToString();
+            textBox1.Text = minxval.ToString();
+            textBox4.Text = maxyval.ToString();
+            textBox3.Text = minyval.ToString();
+                gList.Add(yval, xval);
+                chart1.Series[0].Points.AddXY(0, rpos);
+                chart2.Series[0].Points.AddXY(0, lpos);
+                UpdateGraph();
+                gList.Clear();
+        }
+
+        void FetchAvailablePorts()
+        { 
+            String[] ports = SerialPort.GetPortNames();
+            AvailablePortsBox.Items.AddRange(ports);
+        }
+
+        private void ConnectPortButton_Click(object sender, EventArgs e)
+        {
+
+            SerPort= new SerialPort();
+            SerPort.BaudRate = 115200;
+            SerPort.PortName = AvailablePortsBox.Text;
+            SerPort.Parity = Parity.None;
+            SerPort.DataBits = 8;
+            SerPort.StopBits = StopBits.One;
+            SerPort.ReadBufferSize= 1024;
+            SerPort.DataReceived += SerPort_DataReceived;
+
+            try
+            {
+                SerPort.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error");
+            }
+
+            
+        }
+
+        private void SerPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            RecievedData = SerPort.ReadLine();
+
+            this.Invoke(new Action(ProcessingData));
+        }
+
+        private void ProcessingData()
+        {
+
+           if(RecievedData.Contains('<')&&RecievedData.Contains('>'))
+            {
+
+            SplitData = RecievedData.Split(';');
+
+                
+
+            TextBoxValue1.Text= SplitData[1];
+            TextBoxValue2.Text= SplitData[2];
+            TextBoxValue3.Text= SplitData[3];
+            TextBoxValue4.Text= SplitData[4];
+
+            this.Invoke(new Action(ConvertAndFill));
+
+            Array.Clear(SplitData,0,SplitData.Length);
+           }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                SerPort.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error");
+            }
+        }
+
+        private void DisconnectPortButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SerPort.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+        }
+    }
+
+
+}
